2194d763a23a4756c2c10e162c2db780b580b35f
Alexei Spirit <alexeispirit@gmail.com>
2020-05-28 14:47:29 +0500

    31-multiple-types-in-arrays

--- 03-annotations/arrays.ts
+++ 03-annotations/arrays.ts
@@ -14,3 +14,7 @@ const myCar = carMakers.pop();
 carMakers.map((car: string): string => {
   return car.toUpperCase();
 });
+
+// Flexible types
+const importantDates: (Date | string)[] = [new Date(), "2030-10-10"];
+importantDates.push("2030-10-20");
