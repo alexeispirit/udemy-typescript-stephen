ccefb9274c15b9c87bbe39874608b0aca4eb3412
Alexei Spirit <alexeispirit@gmail.com>
2020-05-28 15:11:56 +0500

    41-code-reuse-with-interfaces

--- 03-annotations/interfaces.ts
+++ 03-annotations/interfaces.ts
@@ -15,8 +15,18 @@ const oldCivic = {
   },
 };
 
+const pepsiDrink = {
+  color: "brown",
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} grams of sugar`;
+  },
+};
+
 const printSummary = (item: Reportable): void => {
   console.log(item.summary());
 };
 
 printSummary(oldCivic);
+printSummary(pepsiDrink);
