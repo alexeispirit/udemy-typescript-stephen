cbb11d64e5a919b2f61b6978c369b46acd4401b7
Alexei Spirit <alexeispirit@gmail.com>
2020-05-29 09:47:18 +0500

    83-separating-swapping-and-comparison

--- 05-sort/src/index.ts
+++ 05-sort/src/index.ts
@@ -1,3 +1,7 @@
-// const sorter = new Sorter([10, 3, -5, 0]);
-// sorter.sort();
-// console.log(sorter.collection);
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
+
+const numbers = new NumbersCollection([10, 3, -5, 0]);
+const sorter = new Sorter(numbers);
+sorter.sort();
+console.log(numbers.data);
