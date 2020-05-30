a8cb95c1ec4ebbacb0751aa988b15c50c6a6eed9
Alexei Spirit <alexeispirit@gmail.com>
2020-05-30 18:05:08 +0500

    136-generic-constraints

--- 02-features/generics.ts
+++ 02-features/generics.ts
@@ -47,3 +47,28 @@ function printAnything<T>(arr: T[]): void {
 
 printAnything<string>(["a", "b", "c", "d"]);
 printAnything(["a", "b", "c", "d"]);
+
+// Generic constraits
+class Car {
+  print() {
+    console.log("I am a Car");
+  }
+}
+
+class House {
+  print() {
+    console.log("I am a House");
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+function printHousesOrCars<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
+
+printHousesOrCars<House>([new House(), new House()]);
