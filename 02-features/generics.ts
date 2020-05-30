aa013c78895ee4bc098a3d4b008b9e835f8f33c1
Alexei Spirit <alexeispirit@gmail.com>
2020-05-30 18:01:15 +0500

    135-function-generics

--- 02-features/generics.ts
+++ 02-features/generics.ts
@@ -24,3 +24,26 @@ class ArrayOfAnything<T> {
 
 // const arr = new ArrayOfAnything<string>(["a", "b", "c", "d"]);
 const arr = new ArrayOfAnything(["a", "b", "c", "d"]);
+
+// Generics with functions
+
+function printStrings(arr: string[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printNumbers(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+printAnything<string>(["a", "b", "c", "d"]);
+printAnything(["a", "b", "c", "d"]);
