4015371997065fb57e00c257b53cadb42b61636f
Alexei Spirit <alexeispirit@gmail.com>
2020-05-30 22:09:26 +0500

    142-optional-interface-properties

--- 07-web/src/index.ts
+++ 07-web/src/index.ts
@@ -2,5 +2,7 @@ import { User } from "./models/User";
 
 const user = new User({ name: "alex", age: 30 });
 
+user.set({ name: "newname" });
+
 console.log(user.get("name"));
 console.log(user.get("age"));
