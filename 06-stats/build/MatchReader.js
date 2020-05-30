171deaf8941b8e47e80163a583a3cfb62847c7ed
Alexei Spirit <alexeispirit@gmail.com>
2020-05-30 11:53:01 +0500

    118-extracting-match-references

--- 06-stats/build/MatchReader.js
+++ 06-stats/build/MatchReader.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var MatchReader = /** @class */ (function () {
     function MatchReader(reader) {
         this.reader = reader;
