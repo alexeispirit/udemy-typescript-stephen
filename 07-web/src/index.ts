7aff0d2e7aa73a7a2c67491ce1f18f17c5582967
Alexei Spirit <alexeispirit@gmail.com>
2020-05-30 22:21:25 +0500

    147-triggering-event-callbacks

--- 07-web/src/index.ts
+++ 07-web/src/index.ts
@@ -2,6 +2,14 @@ import { User } from "./models/User";
 
 const user = new User({ name: "alex", age: 30 });
 
-user.on("change", () => {});
+user.on("change", () => {
+  console.log("change1");
+});
+user.on("change", () => {
+  console.log("change3");
+});
+user.on("save", () => {
+  console.log("save");
+});
 
-console.log(user);
+user.trigger("savess");
