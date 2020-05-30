7558a9e04dbc780e5b7f9520446a43adbc91cf8d
Alexei Spirit <alexeispirit@gmail.com>
2020-05-30 17:43:54 +0500

    129-generating-html-reports

--- 06-stats/build/index.js
+++ 06-stats/build/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var MatchReader_1 = require("./MatchReader");
 var CsvFileReader_1 = require("./CsvFileReader");
-var ConsoleReport_1 = require("./reportTargets/ConsoleReport");
+var HtmlReport_1 = require("./reportTargets/HtmlReport");
 var WinsAnalysis_1 = require("./ananysis/WinsAnalysis");
 var Summary_1 = require("./Summary");
 // Create an object that satisfies the 'DataReader' interface
@@ -11,5 +11,9 @@ var csvFileReader = new CsvFileReader_1.CsvFileReader("football.csv");
 // the 'DataReader' interface
 var matchReader = new MatchReader_1.MatchReader(csvFileReader);
 matchReader.load();
-var summary = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalysis("Man United"), new ConsoleReport_1.ConsoleReport());
+// const summary = new Summary(
+//   new WinsAnalysis("Man United"),
+//   new ConsoleReport()
+// );
+var summary = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalysis("Man United"), new HtmlReport_1.HtmlReport());
 summary.buildAndPrintReport(matchReader.matches);
