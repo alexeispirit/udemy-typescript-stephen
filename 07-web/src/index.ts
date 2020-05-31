d0b5e0c904101442be46ab891f746ab6dc972c8d
Alexei Spirit <alexeispirit@gmail.com>
2020-05-31 11:34:39 +0500

    170-context-issue

--- 07-web/src/index.ts
+++ 07-web/src/index.ts
@@ -2,6 +2,10 @@ import { User } from "./models/User";
 
 const user = new User({ name: "alex", age: 30 });
 
+console.log(user.get("name"));
+
 user.on("change", () => {
   console.log("change");
 });
+
+user.trigger("change");
