1a767222182ef287822c91cc0d6dffec969c6eed
Alexei Spirit <alexeispirit@gmail.com>
2020-06-03 18:56:37 +0500

    264-action-types-enum

--- 10-re-re-ts/src/actions/index.ts
+++ 10-re-re-ts/src/actions/index.ts
@@ -1,13 +1,21 @@
 import axios from "axios";
 import { Dispatch } from "redux";
 
+import { ActionTypes } from "./types";
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 const url = "https://jsonplaceholder.typicode.com/todos";
 
 export const fetchTodos = () => async (dispatch: Dispatch) => {
-  const res = await axios.get(url);
+  const res = await axios.get<Todo[]>(url);
 
   dispatch({
-    type: "FETCH_TODOS",
+    type: ActionTypes.fetchTodos,
     payload: res.data,
   });
 };
