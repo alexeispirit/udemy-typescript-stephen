f0af1979cfa983c1db804674196f038cb3cff022
Alexei Spirit <alexeispirit@gmail.com>
2020-06-03 20:43:49 +0500

    268-connecting-component-to-redux

--- 10-re-re-ts/src/components/App.tsx
+++ 10-re-re-ts/src/components/App.tsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { connect } from "react-redux";
 
-export class App extends React.Component {
+import { Todo, fetchTodos } from "../actions";
+import { StoreState } from "../reducers";
+
+interface AppProps {
+  todos: Todo[];
+  fetchTodos(): any;
+}
+
+class _App extends React.Component<AppProps> {
   render() {
     return <div>Hi there</div>;
   }
 }
+
+function mapStateToProps(state: StoreState): { todos: Todo[] } {
+  return {
+    todos: state.todos,
+  };
+}
+
+export const App = connect(mapStateToProps, { fetchTodos })(_App);
 
 // const App = (props: AppProps): JSX.Element => {
 //   return <div>{props.color}</div>;
