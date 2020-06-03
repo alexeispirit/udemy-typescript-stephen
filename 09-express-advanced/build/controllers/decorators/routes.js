cb9b2d0bcf8eefbbe185d723e191888b23b96f36
Alexei Spirit <alexeispirit@gmail.com>
2020-06-03 14:23:19 +0500

    244-closed-method-sets-with-enums

--- 09-express-advanced/build/controllers/decorators/routes.js
+++ 09-express-advanced/build/controllers/decorators/routes.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.patch = exports.del = exports.post = exports.put = exports.get = void 0;
+exports.del = exports.patch = exports.put = exports.post = exports.get = void 0;
 require("reflect-metadata");
+var Methods_1 = require("./Methods");
 function routeBinder(method) {
     return function (path) {
         return function (target, key, desc) {
@@ -10,8 +11,8 @@ function routeBinder(method) {
         };
     };
 }
-exports.get = routeBinder("get");
-exports.put = routeBinder("put");
-exports.post = routeBinder("post");
-exports.del = routeBinder("delete");
-exports.patch = routeBinder("patch");
+exports.get = routeBinder(Methods_1.Methods.get);
+exports.post = routeBinder(Methods_1.Methods.post);
+exports.put = routeBinder(Methods_1.Methods.put);
+exports.patch = routeBinder(Methods_1.Methods.patch);
+exports.del = routeBinder(Methods_1.Methods.del);
