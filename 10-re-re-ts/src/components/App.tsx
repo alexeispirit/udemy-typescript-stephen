7efbf018ea37f24cf8d6b4c2bd337802a1c669d2
Alexei Spirit <alexeispirit@gmail.com>
2020-06-03 18:48:50 +0500

    262-redux-setup

---
+++ 10-re-re-ts/src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export class App extends React.Component {
+  render() {
+    return <div>Hi there</div>;
+  }
+}
+
+// const App = (props: AppProps): JSX.Element => {
+//   return <div>{props.color}</div>;
+// };
