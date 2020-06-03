f70cf6700003f5a6a38fd142b0006f3b78070378
Alexei Spirit <alexeispirit@gmail.com>
2020-06-03 11:58:38 +0500

    241-proof-of-concept

--- 09-express-advanced/src/index.ts
+++ 09-express-advanced/src/index.ts
@@ -4,12 +4,16 @@ import cookieSession from "cookie-session";
 
 import { router } from "./routes/loginRoutes";
 
+import { router as controllerRouter } from "./controllers/decorators/controller";
+import "./controllers/LoginController";
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ["sdlkfmdsmfsdkmwer"] }));
 
 app.use(router);
+app.use(controllerRouter);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
